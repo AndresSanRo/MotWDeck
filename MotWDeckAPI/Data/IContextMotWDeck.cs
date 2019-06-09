081ef254f1a98b1577ec87b68cbef039d0becdd2
Andrés <andressanchezro@hotmail.com>
2019-06-09 22:10:44 +0200

    add DbContext and interface for IoC

---
+++ MotWDeckAPI/Data/IContextMotWDeck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotWDeckAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotWDeckAPI.Data
+{
+    public interface IContextMotWDeck
+    {
+        DbSet<Playbook> Playbooks { get; set; }
+        int SaveChanges();
+    }
+}
