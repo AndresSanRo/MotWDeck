ab4455129e179d1aef8c5c8f9a0a63df6218cefc
Andrés <andressanchezro@hotmail.com>
2019-06-14 19:39:48 +0200

    add controller

---
+++ MotWDeckAPI/Controllers/ImprovementController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MotWDeckAPI.Models;
+using MotWDeckAPI.Repositories;
+
+namespace MotWDeckAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImprovementController : ControllerBase
+    {
+        IRepositoryMotWDeck Repo;
+        public ImprovementController(IRepositoryMotWDeck Repo)
+        {
+            this.Repo = Repo;
+        }
+        [HttpGet]
+        public List<Improvement> Get()
+        {
+            return Repo.GetImprovements();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public Improvement Get(String id)
+        {
+            return Repo.GetImprovement(int.Parse(id));
+        }
+    }
+}
