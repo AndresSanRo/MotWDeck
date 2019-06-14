44e41b546cf5acb8b4b03dd6b729d4720bc84608
Andrés <andressanchezro@hotmail.com>
2019-06-14 19:38:34 +0200

    add improvement model

---
+++ MotWDeckAPI/Models/Improvement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotWDeckAPI.Models
+{
+    [Table("IMPROVEMENTS")]
+    public class Improvement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("ID")]
+        public int ID { get; set; }
+        [Column("IMP_NAME")]
+        public String Name { get; set; }
+        [Column("ADVANCED")]
+        public bool Advanced { get; set; }
+    }
+}
