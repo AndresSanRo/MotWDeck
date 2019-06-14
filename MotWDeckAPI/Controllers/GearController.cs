f737da550cdd26f467442c78dc26136db8c2459e
Andrés <andressanchezro@hotmail.com>
2019-06-14 20:03:50 +0200

    add controller

---
+++ MotWDeckAPI/Controllers/GearController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MotWDeckAPI.Models;
+using MotWDeckAPI.Repositories;
+
+namespace MotWDeckAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GearController : ControllerBase
+    {
+        IRepositoryMotWDeck Repo;
+        public GearController(IRepositoryMotWDeck Repo)
+        {
+            this.Repo = Repo;
+        }
+        [HttpGet]
+        public List<GearItem> Get()
+        {
+            return Repo.GetGear();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public GearItem Get(String id)
+        {
+            return Repo.GetGearItem(int.Parse(id));
+        }
+        [HttpPut]
+        public void Put([FromBody] GearItem newItem)
+        {
+            Repo.InsertGearItem(newItem.Name, newItem.Tags);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+            Repo.DeleteGearitem(id);
+        }
+    }
+}
